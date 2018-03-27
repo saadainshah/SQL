e29a83ab9b06b279bf0188956a72e2cc7efd3cfa
saadain taimuri <saadainshah@hotmail.com>
2018-03-27 19:48:19 -0400

    Created 2 Tables

---
+++ SQL_Training.sql
@@ -0,0 +1,24 @@
+--SELECT sysdate FROM dual;
+--SELECT * FROM all_tables;
+CREATE TABLE Departments  
+(  
+EmployeeID number(10),  
+FirstName varchar(255),  
+LastName varchar(255),  
+Email varchar(255),  
+AddressLine varchar(255),  
+City varchar(255)  
+); 
+
+CREATE TABLE Employee  
+(  
+EmployeeID number(10),  
+FirstName varchar(255),  
+LastName varchar(255),  
+Email varchar(255),  
+AddressLine varchar(255),  
+City varchar(255)  
+); 
+
+--DESC EMPLOYEE;
+--DROP TABLE employee;
